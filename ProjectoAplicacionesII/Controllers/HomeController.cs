8e97df3707dd9303d82740c235527e7381b8a2ee
Duvan-C <duvancifuentes204@gmail.com>
2021-05-29 10:24:03 -0500

    Proyecto Aplicaciones

---
+++ ProjectoAplicacionesII/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectoAplicacionesII.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Vision()
+        {
+            return View();
+        }
+
+        public ActionResult Productos()
+        {
+            return View();
+        }
+
+        public ActionResult Ofertas()
+        {
+            return View();
+        }
+    }
+
+}
